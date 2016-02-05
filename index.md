640a95c7bbc8fcf8163c474ac8bb5f4cf062b7e6
Abhishek Nagekar <abhisheknagekar@gmail.com>
2016-02-05 19:08:29 +0530

    ajaxified

--- index.md
+++ index.md
@@ -8,7 +8,7 @@ title: Sometimes, I write..
 
 <ul class="posts">
   {% for post in site.posts %}
-    <li><a href="{{ BASE_PATH }}{{ post.url }}">{{ post.title }}</a> <small><a style="color:grey" href="{{ BASE_PATH }}{{ post.url }}#disqus_thread"></a></small></li>
+    <li><a href="{{ BASE_PATH }}#{{ post.url }}">{{ post.title }}</a> <small><a style="color:grey" href="{{ BASE_PATH }}{{ post.url }}#disqus_thread"></a></small></li>
   {% endfor %}
 </ul>
 
---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+document.addEventListener('click', function(e) {
+    if(e.target['hash']) {
+    
+        // grab the hash
+        targetURL = e.target['hash'].substr(1);
+        xmlObj = new XMLHttpRequest();
+    
+        // on return of data
+        xmlObj.addEventListener('load', function(resp) {
+            // change the entire DOM
+            document.write(resp.srcElement.responseText);
+            
+            // push the new URL into the address bar
+            window.history.pushState('', '', resp.srcElement.responseURL);
+        });
+        
+        xmlObj.open('GET', targetURL);
+        xmlObj.send();
+    }
+});
