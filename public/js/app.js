34e1b089dd77501d41503cd8fce8ac30e781a579
Abhishek Nagekar <abhisheknagekar@gmail.com>
2016-02-05 19:59:25 +0530

    ajaxfying

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,9 @@ document.addEventListener('click', function(e) {
         // on return of data
         xmlObj.addEventListener('load', function(resp) {
             // change the entire DOM
+            document.open();
             document.write(resp.srcElement.responseText);
+            document.close();
             
             // push the new URL into the address bar
             window.history.pushState('', '', resp.srcElement.responseURL);
