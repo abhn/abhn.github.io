a598a58a05f1a52cd2681e915926ca4da98545eb
Abhishek Nagekar <abhishek@nagekar.com>
2020-02-02 20:09:39 +0100

    .

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 let light = 'Light',
   dark = 'Dark';
 
-document.addEventListener("DOMContentLoaded", event => {
+document.addEventListener("DOMContentLoaded", () => {
   init();
 });
 
@@ -12,13 +12,13 @@ const init = () => {
     darkSwitch.addEventListener('click', handleNightModeToggle);
   });
   checkCookie();
-}
+};
 
 const checkCookie = () => {
   if(getCookie('nightMode')) {
     toggle();
   }
-}
+};
 
 const handleNightModeToggle = () => {
   if(getCookie('nightMode')) {
@@ -27,21 +27,21 @@ const handleNightModeToggle = () => {
     setCookie('nightMode', true, 365);
   }
   toggle();
-}
+};
 
 const toggle = () => {
   const darkSwitchesArray = document.querySelectorAll('.dark-light-switch');
   darkSwitchesArray.forEach(darkSwitch => {
     darkSwitch.innerHTML = getCookie('nightMode') ? light : dark;
-  })
+  });
   document.body.classList.toggle('dark');
-}
+};
 
 // https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
 function setCookie(name,value,days) {
-  var expires = "";
+  let expires = "";
   if (days) {
-    var date = new Date();
+    let date = new Date();
     date.setTime(date.getTime() + (days*24*60*60*1000));
     expires = "; expires=" + date.toUTCString();
   }
@@ -49,12 +49,12 @@ function setCookie(name,value,days) {
 }
 
 function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  let nameEQ = name + "=";
+  let ca = document.cookie.split(';');
+  for(let i=0;i < ca.length;i++) {
+    let c = ca[i];
+    while (c.charAt(0) ===' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
   }
   return null;
 }
